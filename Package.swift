588f5305ad90b82d06faa8257bf2722ad2f9a290
rarest automation <automation@noreply.rarestype.com>
2026-04-02 21:22:22 +0000

    Release 1.0.4

--- Package.swift
+++ Package.swift
@@ -25,20 +25,20 @@ let package: Package = .init(
 var DollupBinary: Target {
     var url: String {
         #if os(macOS)
-        "https://download.rarestype.com/dollup/1.0.3/macOS-arm64/dollup.artifactbundle.zip"
+        "https://download.rarestype.com/dollup/1.0.4/macOS-arm64/dollup.artifactbundle.zip"
         #elseif arch(x86_64)
-        "https://download.rarestype.com/dollup/1.0.3/Linux-x86_64/dollup.artifactbundle.zip"
+        "https://download.rarestype.com/dollup/1.0.4/Linux-x86_64/dollup.artifactbundle.zip"
         #else
-        "https://download.rarestype.com/dollup/1.0.3/Linux-aarch64/dollup.artifactbundle.zip"
+        "https://download.rarestype.com/dollup/1.0.4/Linux-aarch64/dollup.artifactbundle.zip"
         #endif
     }
     var checksum: String {
         #if os(macOS)
-        "c81ba4ce8cda23ddd586bdac6c9d29fa06fe23f2fa23684bba4142f184506b7f"
+        "0331291f4a60453eaee25c899f9e469f29cc545cb8f6400be138a306b0b10c80"
         #elseif arch(x86_64)
-        "4b1d929460fca0e8a1931830b95a751734d9b654a3749011ca25f55e46d5c5b6"
+        "f7e40821c900b1b9f8f94c233ff90a9b87c814bf2d5091cfa3c672ac8b3fec44"
         #else
-        "ba1a57fe2eda103b8f15a6de606bcce9a2caec50ecfa5ee2cfb0578f4b172f0b"
+        "94c30cc1892e2e36c4f63e52385b9bf4de81371143f2eed25bb7734f279f9979"
         #endif
     }
 
